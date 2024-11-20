dd1e2159f4db717a2bad490d53383853a39dcad8
ErickGC546 <erick.galindo.c@tecsup.edu.pe>
2024-11-20 01:32:12 -0500

    Initial Commit

---
+++ CalculoCTS/CalculoCTSApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculoCTSApp.swift
+//  CalculoCTS
+//
+//  Created by Mac05 on 20/11/24.
+//
+
+import SwiftUI
+
+@main
+struct CalculoCTSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
